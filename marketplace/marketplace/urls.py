df6b71a072169a76f589a6d99bec63543efd04c9
AriyoX <ahumuzaariyo@gmail.com>
2024-04-11 14:54:18 +0300

    Added log in and sign up functionality.

--- marketplace/marketplace/urls.py
+++ marketplace/marketplace/urls.py
@@ -22,8 +22,7 @@ from django.conf.urls.static import static
 from base.views import index, contact
 
 urlpatterns = [
-    path('',index, name='index'),
+    path('', include('base.urls')),
     path('items/', include('item.urls')),
-    path('contact/',contact, name='contact'),
     path("admin/", admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
