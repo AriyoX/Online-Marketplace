ecd7d60444c760ce9982341b8d2f37e6ada961b7
AriyoX <ahumuzaariyo@gmail.com>
2024-04-15 19:32:43 +0300

    Added dashboard.

--- marketplace/base/views.py
+++ marketplace/base/views.py
@@ -6,6 +6,7 @@ from .forms import SignUpForm
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
     categories = Category.objects.all()
+    
     return render(request, 'base/index.html', {
         'categories': categories,
         'items': items,
--- marketplace/marketplace/settings.py
+++ marketplace/marketplace/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'base',
     'item',
+    'dashboard',
 ]
 
 MIDDLEWARE = [
