c2b7b2c34fcece3573286de9b76b80bc43d1932c
AriyoX <ahumuzaariyo@gmail.com>
2024-04-16 14:25:07 +0300

    Added edit button and functionality.

--- marketplace/item/views.py
+++ marketplace/item/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 
-from .forms import NewItemForm
+from .forms import NewItemForm, EditItemForm
 from .models import Item
 
 # Create your views here.
@@ -29,7 +29,6 @@ def new(request):
     else:
         form = NewItemForm()
 
-
     return render(request, 'item/form.html', {
         'form': form,
         'title': 'New Item',
@@ -41,3 +40,24 @@ def delete(request, pk):
     item.delete()
 
     return redirect('dashboard:index')
+
+@login_required
+def edit(request, pk):
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance=item)
+
+        if form.is_valid():
+            item.save()
+
+            return redirect('item:detail', pk=item.id)
+        
+    else:
+        # to avoid passing empty data, we need to use the same instance that we are editing
+        form = NewItemForm(instance=item)
+
+
+    return render(request, 'item/form.html', {
+        'form': form,
+        'title': 'Edit Item',
+    })
